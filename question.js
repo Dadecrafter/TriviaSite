96ff88b1cf45780dc4f14e7dfaa5220fcb7a8c6b
Dadecrafter <39542782+Dadecrafter@users.noreply.github.com>
2020-05-16 14:27:00 -0600

    Create question.js

---
+++ question.js
@@ -0,0 +1,81 @@
+
+
+
+const quiz = [
+    {
+        q:'Where in the human body would you find the medulla oblongata?',
+        options:['The Brain','The Heart','The Liver','The Lungs'],
+        answer:0
+    },
+    {
+        q:'What is the best selling novel of all time?',
+        options:['A Tale of Two Cities','Moby Dick','Don Quixote','To Kill a Mockingbird'],
+        answer:2
+    },
+    {
+        q:'What musical term indicates a chord where the notes are played one after another rather than all together?',
+        options:['Fermata','Arpeggio','Chord','Accent'],
+        answer:1
+    },
+    {
+        q:'Which gas is formed after a hydrogen bomb is detonated?',
+        options:['Helium','Oxygen','Neon','Argon'],
+        answer:0
+    },
+    {
+        q:'What color does gold leaf appear if you hold it up to the light?',
+        options:['Yellow','Red','Blue','Green'],
+        answer:3
+    },
+    {
+        q:'What novel is set on a desert planet inhabited by giant sandworms?',
+        options:['Dune','Solaris','The Drowned World','The Left Hand of Darkness'],
+        answer:0
+    },
+    {
+        q:'Good King Wenceslas is a popular Christmas carol. But of which German region, now part of Czechoslovakia, was Wenceslas king?',
+        options:['Bohemia','Prague','Brno','Pilsen'],
+        answer:0
+    },
+    {
+        q:'“Les Fauves” was a name given to a group of artists that included Matisse. What is its literal meaning?',
+        options:['The Wild Beasts','The Miserable Ones','The Big Cats','The Sad Few'],
+        answer:0
+    },
+    {
+        q:'Who collaborated with Karl Marx to produce The Communist Manifesto?',
+        options:['Freidrich Engels','Vladimir Lenin','Nikita Krushchev','Leon Trotsky'],
+        answer:0
+    },
+    {
+        q:'Which Stephen King novel takes place mostly in the fictional Overlook Hotel?',
+        options:['The Shining','Pet Semetary','It','Carrie'],
+        answer:0
+    },
+    {
+        q:'Who is the main protaganist of The Hunger Games?',
+        options:['Katniss Everdeen','Peeta Mellark','Haymitch Abernathy','Gale Hawthorne'],
+        answer:0
+    },
+    {
+        q:'At the beginning of Lord of the Flies, which character controls the boys’ only means of making a fire?',
+        options:['Piggy','Ralph','Simon','Jack'],
+        answer:0
+    },
+    {
+        q:'What is the adverb in the following sentence: "He ran quickly in last year’s race?"',
+        options:['Quickly','Last','Ran','He'],
+        answer:0
+    },
+    {
+        q:'In the narrative poem “Dante’s Inferno” what words can be found above the Gate of Hell?',
+        options:['Abandon all hope, ye who enter here','Stiffness of death','Let the superior respond','Eternal rest'],
+        answer:0
+    },
+    
+    {
+        q:'Who is the epic hero in Homer\' The Odyssey?',
+        options:['Odysseus','Heracles','Perseus','Aeneas'],
+        answer:0
+    },
+]
